71535888ac8677db9c4ede5e62da110b42c79bfe
Fidahasm <fidahkt@gmail.com>
2024-08-10 01:47:14 +0530

    student portal design
    The ui for the student portal designing completed.

--- certificate-main/src/Components/Body/Body.js
+++ certificate-main/src/Components/Body/Body.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../Body/Body.css'
-function Body() {
+const Body = ({children}) => {
   return (
     <div>
             <div className='body-main'>
-              <div className='body-card'></div>
+              <div className='body-card'>
+                {children}
+              </div>
             </div>
 
     </div>
   )
 }
-
 export default Body
