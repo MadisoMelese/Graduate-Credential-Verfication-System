960e12df4164447b0fc6fc99153d81cca78a655d
Fidahasm <fidahkt@gmail.com>
2024-08-09 23:15:47 +0530

    body component completed

---
+++ certificate-main/src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Body/Body.css'
+function Body() {
+  return (
+    <div>
+            <div className='body-main'>
+              <div className='body-card'></div>
+            </div>
+
+    </div>
+  )
+}
+
+export default Body
--- certificate-main/src/Screen/StudentPortal/StudentPage.js
+++ certificate-main/src/Screen/StudentPortal/StudentPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../StudentPortal/StudentPage.css'
+import Body from '../../Components/Body/Body'
 
 function StudentPage() {
   return (
     <div>
-      
+      <Body />
     </div>
   )
 }
