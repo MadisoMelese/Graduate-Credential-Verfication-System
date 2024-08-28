f65c97192013d56fb9eb3df2ecd986efc63bbbb1
Fidah Abdul Shukoor Muhammed <128781740+Fidahasm@users.noreply.github.com>
2024-08-28 21:49:54 +0530

    Update LoginPage.js

--- certificate-main/src/Screen/LoginPage/LoginPage.js
+++ certificate-main/src/Screen/LoginPage/LoginPage.js
@@ -7,11 +7,11 @@ import axios from 'axios';
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+axios.defaults.withCredentials=true;
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const res = await axios.post('http://localhost:5000/login', { username, password });
+        const res = await axios.post('https://certificate-verificationn-system-api.vercel.app/login', { username, password });
         localStorage.setItem('token', res.data.token);
         if (res.data.role === 'admin') {
             window.location.href = '/admin';
